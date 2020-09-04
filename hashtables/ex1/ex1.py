e39bdf997b76bda9486e8b709753ee788e0789e2
zakaria touzri <zakadmaster@gmail.com>
2020-09-04 17:25:01 +0200

    ADDED: ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,22 @@
+from hashtable import HashTable
+
 def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
     # Your code here
-
+    obj = {}
+    for i in range(length):
+        obj[weights[i]] = i
+    for i in range(length):
+        if (limit - weights[i]) in obj:
+            if limit == weights[i] * 2:
+                arr = []
+                for o in range(length):
+                    if weights[o] == weights[i]:
+                        arr.append(length - o - 1)
+                return arr
+            return [obj[limit - weights[i]], obj[weights[i]]]
     return None
+
+print(get_indices_of_item_weights([ 4, 4 ], length = 2, limit = 8))
