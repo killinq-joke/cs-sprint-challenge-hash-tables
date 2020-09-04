03425e6617376f690b152e01110761bfeece51e8
zakaria touzri <zakadmaster@gmail.com>
2020-09-04 17:55:33 +0200

    ADDED: ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,22 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
-
+    route = []
+    obj = {}
+    for i in tickets:
+        obj[i.source] = i.destination
+    key = obj["NONE"]
+    route.append(key)
+    while obj[key] != "NONE":
+        route.append(obj[key])
+        key = obj[key]
+    route.append(obj[key])
     return route
+
+ticket_1 = Ticket("NONE", "PDX")
+ticket_2 = Ticket("PDX", "DCA")
+ticket_3 = Ticket("DCA", "NONE")
+
+tickets = [ticket_1, ticket_2, ticket_3]
+
+print(reconstruct_trip(tickets, len(tickets)))
