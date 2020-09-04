4f920edccd1c4ae33cb2937cdb0486b43063f35b
zakaria touzri <zakadmaster@gmail.com>
2020-09-04 17:55:43 +0200

    ADDED: ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,16 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    obj = {}
 
-    return result
+    for i in range(len(arrays)):
+        for j in range(len(arrays[i])):
+            if arrays[i][j] in obj:
+                obj[arrays[i][j]].append(i)
+            else:
+                obj[arrays[i][j]] = [i]
+    return obj
+
 
 
 if __name__ == "__main__":
@@ -14,4 +22,4 @@ if __name__ == "__main__":
     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+print(intersection(arrays))
